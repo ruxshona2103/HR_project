b7421545fc48ce7b27127b77b67447f57d780e12
Khidirov Ali <alihidirov285@gmail.com>
2026-02-15 12:28:16 +0600

    yangilandi

--- apps/interviews/apps.py
+++ apps/interviews/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class InterviewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'interviews'
+    name = 'apps.interviews'
