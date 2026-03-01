e4a2b1b09581b30d8d6916c505fae9dfb7c03280
Khidirov Ali <alihidirov285@gmail.com>
2026-03-01 14:44:31 +0600

    drf_spectacularga O'TILDI

--- config/urls.py
+++ config/urls.py
@@ -1,41 +1,17 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
 from rest_framework import permissions
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from drf_yasg.generators import OpenAPISchemaGenerator
-
-
-class JWTSchemaGenerator(OpenAPISchemaGenerator):
-    def get_security_definitions(self):
-        return {
-            "Bearer": {
-                "type": "apiKey",
-                "name": "Authorization",
-                "in": "header",
-                "description": "Tokenni quyidagi formatda kiriting: Bearer <sizning_tokeningiz>",
-            }
-        }
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="API HR",
-        default_version="v1",
-        description="HR PROJECT API hujjatlari",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email=settings.DEFAULT_FROM_EMAIL if hasattr(settings, "DEFAULT_FROM_EMAIL") else ""),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-    generator_class=JWTSchemaGenerator,
+from django.views.generic import RedirectView
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
 )
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/swagger/"), name="home"),
     path("admin/", admin.site.urls),
 
     path("api/accounts/", include("apps.accounts.urls")),
@@ -44,22 +20,9 @@ urlpatterns = [
     path("api/vacancies/", include("apps.vacancies.urls")),
     path("api/interviews/", include("apps.interviews.urls")),
 
-    # Swagger
-    re_path(
-        r"^swagger(?P<format>\.json|\.yaml)$",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path(
-        "redoc/",
-        schema_view.with_ui("redoc", cache_timeout=0),
-        name="schema-redoc",
-    ),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
 
 if settings.DEBUG:
