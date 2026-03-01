891331476a0dcc62bd1a5918d837d6e6afb72c96
Khidirov Ali <alihidirov285@gmail.com>
2026-03-01 14:51:01 +0600

    taglarga bo'lindi

--- apps/vacancies/views.py
+++ apps/vacancies/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from drf_spectacular.utils import extend_schema
 from rest_framework import viewsets
 from .models import Vacancy
 from .serializers import VacancySerializer
 
+
+@extend_schema(tags=["Vacancies"])
 class VacancyViewSet(viewsets.ModelViewSet):
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
