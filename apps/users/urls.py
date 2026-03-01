73c1fdb0e386e2ec73001bda23a8d1cd523d198b
Khidirov Ali <alihidirov285@gmail.com>
2026-03-01 14:46:51 +0600

    token refresh sozlandi

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,8 @@
-from rest_framework.routers import DefaultRouter
-
-from .views import LogoutView, MeView, VerifyOTPView, BotLinkView
+from drf_spectacular.utils import extend_schema
+from .views import LogoutView, MeView, VerifyOTPView, BotLinkView, OTPTokenRefreshView
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
+
 
 
 urlpatterns = [
@@ -10,5 +10,6 @@ urlpatterns = [
     path('auth/verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
     path('auth/me/', MeView.as_view(), name='me'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token-refresh'),
+    path('auth/token/refresh/', OTPTokenRefreshView.as_view(), name='token-refresh'),
+
 ]
