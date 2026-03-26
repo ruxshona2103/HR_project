57d83c9ce64772e1956e829ebe4abdc93aae1c57
Khidirov Ali <alihidirov285@gmail.com>
2026-03-26 16:58:27 +0600

    app delete qilindi

--- apps/users/serializers/otp.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-
-
-class VerifyOTPSerializer(serializers.Serializer):
-    phone_number = serializers.CharField(max_length=13)
-    code = serializers.CharField(max_length=6)
-
