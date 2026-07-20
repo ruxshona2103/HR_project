bfa4fb8c3e406ad822c496041aa69d6e71486865
Khidirov Ali <alihidirov285@gmail.com>
2026-07-20 22:57:07 +0500

    keraksiz applar delete qilindi

--- apps/candidates/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import get_my_profile, ai_resume_check
-
-urlpatterns = [
-    path('profile/', get_my_profile, name='candidate-profile'),
-    path('ai-check/', ai_resume_check, name='ai-resume-check'),
-]
