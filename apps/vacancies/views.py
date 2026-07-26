cdda075293c410dc63de83a7c95c0d1783bae021
Khidirov Ali <alihidirov285@gmail.com>
2026-07-26 19:48:21 +0500

    All possible bugs have been fixed and it is ready for production.

--- apps/vacancies/views.py
+++ apps/vacancies/views.py
@@ -1,14 +1,21 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework import viewsets
+from rest_framework import viewsets, serializers
+from rest_framework.permissions import IsAuthenticated
 from .models import Vacancy
 from .serializers import VacancySerializer
-
+from .permissions import IsVacancyOwnerOrReadOnly
 
 @extend_schema(tags=["Vacancies"])
 class VacancyViewSet(viewsets.ModelViewSet):
-    permission_classes = []
+    permission_classes = [IsAuthenticated, IsVacancyOwnerOrReadOnly]
     queryset = Vacancy.objects.all()
     serializer_class = VacancySerializer
 
     def get_serializer_context(self):
         return {'request': self.request}
+
+    def perform_create(self, serializer):
+        company = getattr(self.request.user, 'company_profile', None)
+        if not company:
+            raise serializers.ValidationError({"company": "Sizda kompaniya profili mavjud emas."})
+        serializer.save(company=company)
