70694af226276fc39da368d31d6c63fd9d6b848c
Khidirov Ali <alihidirov285@gmail.com>
2026-07-26 19:57:55 +0500

    All possible bugs have been fixed and it is ready for production.

--- apps/ai_engine/urls.py
+++ apps/ai_engine/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ResumeCheckAPIView, InterviewStartAPIView
+from .views import ResumeCheckAPIView, InterviewStartAPIView, get_interview_status, get_ai_feedback
+
 
 urlpatterns = [
-
     path('resume-check/', ResumeCheckAPIView.as_view(), name='ai-resume-check'),
     path('start-interview/<int:vacancy_id>/', InterviewStartAPIView.as_view(), name='ai-interview-start'),
+    path('status/<int:vacancy_id>/', get_interview_status, name='ai-interview-status'),
+    path('feedback/<int:result_id>/', get_ai_feedback, name='ai-interview-feedback'),
 ]
