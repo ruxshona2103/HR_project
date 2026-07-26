acba92c3bc7a99459621a4734891aad51c48430b
Khidirov Ali <alihidirov285@gmail.com>
2026-07-26 19:47:48 +0500

    All possible bugs have been fixed and it is ready for production.

--- apps/user_profile/serializers.py
+++ apps/user_profile/serializers.py
@@ -2,7 +2,7 @@ from apps.user_profile.models import UserProfile
 from rest_framework import serializers
 
 
-class UserProfileSerializer(serializers.ModelSerializer):
+class UserProfileSerializer1(serializers.ModelSerializer):
     class Meta:
         model = UserProfile
         fields = ['id', 'first_name', 'last_name', 'birth_date', 'phone_number', 'university_name', 'degree', 'course', 'field_of_study']
